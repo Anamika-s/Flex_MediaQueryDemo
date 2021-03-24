49655d6b202b8262c51265195acf1e1f8087004d
Anamika <anamika.sawhney22@gmail.com>
2021-03-24 16:51:53 +0530

    Added on 24th March

---
+++ FlexBoxDemo/wwwroot/Functions.js
@@ -0,0 +1,53 @@
+﻿function DisplayNumbers() {
+    var str = "";
+    for (var x = 1; x <= 10; x++) {
+        str += x + "\n";
+
+    }
+    alert(str);
+}
+function CheckForPrime() {
+    var num = parseInt(document.getElementById("txtNo").value);
+    var flag = 0;
+    for (var x = 2; x <= num / 2; x++) {
+        if (num % x == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1) {
+        alert("Its not prime");
+    }
+    else {
+        alert("Its prime");
+    }
+
+}
+function DisplayHello() {
+    alert("This is a message from head section");
+}
+function AddNumbers() {
+    var num1 = parseInt(document.getElementById("txtNo1").value);
+    var num2 = parseInt(document.getElementById("txtNo2").value);
+    document.getElementById("txtResult").value =
+        num1 + num2;
+
+}
+function SubtractNumbers() {
+    var num1 = parseInt(document.getElementById("txtNo1").value);
+    var num2 = parseInt(document.getElementById("txtNo2").value);
+    document.getElementById("txtResult").value =
+        num1 - num2;
+
+}
+//function one() {
+//    {
+//        const x = 10;
+//        console.log(x);
+
+//    x = 100;
+//    console.log(x);
+//}
+function ConvertToUpper() {
+    alert(txtName.value.toUpperCase());
+}
